56060eef89bc75c772de9dabb0668d7bc4e9c727
Ngawang Tenzin <ngawang.tenzin1@my.jcu.edu.au>
2025-06-27 12:42:33 +1000

    Feature: restructure profile website by removing outdated pages Added a new lite version for quick reference

--- index.js
+++
@@ -1,9 +0,0 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-const checkboxValues = {};
-
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", () => {
-    checkboxValues[checkbox.id] = checkbox.checked;
-    console.log("Checkbox states:", checkboxValues);
-  });
-});
